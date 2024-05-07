72a9475b6bdd62c9ab61b6118e64e651febb11af
terrorbladegodlike <ternovetchii.2002@mail.ru>
2024-05-07 15:55:49 +0300

    Styling of the "Membership" component

--- components/Membership.tsx
+++ components/Membership.tsx
@@ -1,8 +1,42 @@
+'use client'
+
+// Import Motion & Variants
+import { motion } from 'framer-motion'
+import { fadeIn } from '@/lib/variants'
+
+// Import Component
+import MembershipSlider from './MembershipSlider'
+
 const Membership = () => {
    return (
-      <section className='h-screen' id='prices'>
-         Membership
-      </section>
+      <motion.section
+         variants={fadeIn('up', 0.2)}
+         initial='hidden'
+         whileInView={'show'}
+         viewport={{ once: false, amount: 0.2 }}
+         className='py-8 xl:py-0 lg:h-[95vh] bg-membership bg-cover bg-center relative before:w-full before:h-full before:bg-black/50 before:absolute before:top-0 before:z-10'
+         id='prices'
+      >
+         <div className='container mx-auto text-white h-full flex flex-col xl:pt-24 relative z-20'>
+            <motion.h2
+               variants={fadeIn('up', 0.4)}
+               initial='hidden'
+               whileInView={'show'}
+               viewport={{ once: false, amount: 0.2 }}
+               className='h2 text-white text-center mb-8'
+            >
+               Membership
+            </motion.h2>
+            <motion.div
+               variants={fadeIn('up', 0.6)}
+               initial='hidden'
+               whileInView={'show'}
+               viewport={{ once: false, amount: 0.2 }}
+            >
+               <MembershipSlider />
+            </motion.div>
+         </div>
+      </motion.section>
    )
 }
 
