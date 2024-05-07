0064234e921dad4ea663652681c405aa41e6e157
terrorbladegodlike <ternovetchii.2002@mail.ru>
2024-05-07 21:17:27 +0300

    Styling of the "Brands" component

--- components/Brands.tsx
+++ components/Brands.tsx
@@ -1,7 +1,92 @@
+'use client'
+
+// Import Link & Image from Next
+import Image from 'next/image'
+import Link from 'next/link'
+
+// Import Motion
+import { motion } from 'framer-motion'
+
+const brandsData = [
+   {
+      src: '/assets/img/brands/brand-1.png',
+      href: '',
+   },
+   {
+      src: '/assets/img/brands/brand-2.png',
+      href: '',
+   },
+   {
+      src: '/assets/img/brands/brand-3.png',
+      href: '',
+   },
+   {
+      src: '/assets/img/brands/brand-4.png',
+      href: '',
+   },
+   {
+      src: '/assets/img/brands/brand-5.png',
+      href: '',
+   },
+]
+
+const brandContainerVariant = {
+   hidden: {
+      opacity: 0,
+   },
+   show: {
+      opacity: 1,
+      transition: {
+         staggerChildren: 0.4,
+         duration: 0.5,
+         ease: 'linear',
+      },
+   },
+}
+
+const brandItem = {
+   hidden: {
+      y: 20,
+      opacity: 0,
+   },
+
+   show: {
+      opacity: 1,
+      transition: {
+         duration: 0.5,
+         ease: [0.25, 0.6, 0.3, 0.8],
+      },
+   },
+}
+
 const Brands = () => {
    return (
-      <section className='h-[25vh]' id='contact'>
-         Brands
+      <section className='py-8' id='contact'>
+         <div className='container mx-auto'>
+            <motion.div
+               variants={brandContainerVariant}
+               initial='hidden'
+               whileInView={'show'}
+               viewport={{ once: false, amount: 0.3 }}
+               className='grid grid-cols-2 lg:grid-cols-5 py-8'
+            >
+               {brandsData.map((brand, index) => {
+                  return (
+                     <motion.div variants={brandItem} key={index}>
+                        <Link className='group' href={brand.href}>
+                           <Image
+                              className='opacity-50 group-hover:opacity-100 transition-all mx-auto'
+                              src={brand.src}
+                              width={204}
+                              height={106}
+                              alt=''
+                           ></Image>
+                        </Link>
+                     </motion.div>
+                  )
+               })}
+            </motion.div>
+         </div>
       </section>
    )
 }
